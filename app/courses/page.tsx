993e1745cc455ebe0f80ac11819b44eb0e6ece25
kekcode-9 <rupamita.s97@gmail.com>
2024-01-16 22:27:04 +0530

    dummy push

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import CourseCatalogue from "@/components/courses/course-catalogue";
 
 export default function Courses() {
-  return (
+  return ( 
     <CourseCatalogue/>
   );
 }
