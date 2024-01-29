8c2220f5f6e6697968e68e37bdac64f7b2180376
kekcode-9 <rupamita.s97@gmail.com>
2024-01-29 18:32:03 +0530

    og

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,21 @@ import './globals.css';
 const raleway = Raleway({ subsets: ['latin']});
 
 export const metadata: Metadata = {
-  title: 'Esner Academy',
-  description: 'Academy of traditional and digital Arts',
+  title: 'Esner Academy for Traditional & Digital Arts',
+  description: 'A Next.js 13 + Typescript project',
+  openGraph: {
+    title: 'Esner Academy for Traditional & Digital Arts',
+    description: 'A Next.js v13 & Typescript project, designed in Figma.',
+    url: 'https://esneracademy.vercel.app/',
+    siteName: 'Esner Academy',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/dxvx3y6ch/image/upload/f_auto,q_auto/egxirxjmmyk3qsjuikig',
+        width: 2560,
+        height: 1664
+      }
+    ]
+  }
 }
 
 export default function RootLayout({
